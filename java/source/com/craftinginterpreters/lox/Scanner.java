c0aa1023beb9302d4860ca0b651848b9a8e1c61c
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2020-09-07 21:32:38 +0100

    Handle keywords and identifiers

--- java/source/com/craftinginterpreters/lox/Scanner.java
+++ java/source/com/craftinginterpreters/lox/Scanner.java
@@ -1,9 +1,9 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
-// import java.util.HashMap;
+import java.util.HashMap;
 import java.util.List;
-// import java.util.Map;
+import java.util.Map;
 
 // Some people don't like this style but it saves having to do `TokenType dot thing` all over.
 import static com.craftinginterpreters.lox.TokenType.*;
@@ -99,11 +99,31 @@ class Scanner {
       default:
         if (isDigit(c)) {
           number();
+        } else if (isAlpha(c)) {
+          identifier();
         } else {
           Lox.error(line, "Unexpected character.");
         }
         break;
     }
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) {
+      advance();
+    }
+
+    // Grab the identifier
+    String text = source.substring(start, current);
+
+    // Check if the identifier is a reserved word.
+    TokenType type = keywords.get(text);
+    if (type == null) {
+      // It's just a vanilla identifier if not reserved.
+      type = IDENTIFIER;
+    }
+
+    addToken(type);
   }
 
   private void number() {
@@ -172,8 +192,16 @@ class Scanner {
     return source.charAt(current + 1);
   }
 
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+  }
+
   private boolean isDigit(char c) {
     return c >= '0' && c <= '9';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
   }
 
   private boolean isAtEnd() {
@@ -192,5 +220,27 @@ class Scanner {
   private void addToken(TokenType type, Object literal) {
     String text = source.substring(start, current);
     tokens.add(new Token(type, text, literal, line));
+  }
+
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and", AND);
+    keywords.put("class", CLASS);
+    keywords.put("else", ELSE);
+    keywords.put("false", FALSE);
+    keywords.put("for", FOR);
+    keywords.put("fun", FUN);
+    keywords.put("if", IF);
+    keywords.put("nil", NIL);
+    keywords.put("or", OR);
+    keywords.put("print", PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super", SUPER);
+    keywords.put("this", THIS);
+    keywords.put("true", TRUE);
+    keywords.put("var", VAR);
+    keywords.put("while", WHILE);
   }
 }
